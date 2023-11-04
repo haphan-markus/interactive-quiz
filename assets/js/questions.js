f5dfccd32910d60c0e9d978438bbd5fd6626fc67
Ha Phan <ha.markus.phan@gmail.com>
2023-11-04 11:20:59 +0000

    display/ undisplay start-screen div

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById('start');
+var startScreen = document.getElementById('start-screen');
+
+startButton.addEventListener('click',function(event){
+    startScreen.style.display = 'none';
+});
+
+// Upon clicking the Start button, it will prompt to the questions function
+
+function questions(){
+
+}
+
+//Store the lists of questions in an array, or object, with each question is an object
+// Each question has 
