2ea059c567a14bfcb0c682bc3b1739c3f5925a55
Ha Phan <ha.markus.phan@gmail.com>
2023-11-05 21:21:10 +0000

    set the clock to stop when reaching zero or all questions are answered

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,13 +9,28 @@ var quizLength = quiz.length;
 console.log("Total number of questions in the quiz: " + quizLength);
 
 // Show time left for answering and show countdown
-var timeLeft = 120;
+var timeLeft = 10;
 var time = document.querySelector('#time');
 time.innerHTML = timeLeft;
 
 startButton.addEventListener('click',answerQuestion);
 
 function answerQuestion(event){
+    // Set coundown function
+    var timeInterval = setInterval(function(){
+        timeLeft--;
+        time.innerHTML = timeLeft;
+        if (timeLeft === 0){
+            // Stop execution of time countdown
+            clearInterval(timeInterval);
+        }
+    },1000);
+    
+    // if (timeLeft === 0){
+    //     // Stop execution of time countdown
+    //     clearInterval(timeInterval);
+    // }
+    
     // Hide start-screen div
     var startScreenState = startScreen.getAttribute('data-state');
     var startScreenClass = startScreen.getAttribute('class');
@@ -74,6 +89,7 @@ function answerQuestion(event){
                 console.log('Total correct answers:' + correctAnswer);
                 } else {
                 paraEl.innerText = "Wrong!";
+                timeLeft = timeLeft - 10;
                 wrongAnswer ++;
                 console.log("Total wrong answers: " + wrongAnswer);
             }
@@ -81,16 +97,17 @@ function answerQuestion(event){
             setTime(paraEl);
             // Increase the value of k to switch to new Question
             k ++;
-
+            console.log(k);
             // Add if conditional statement to break this loop when all questions have been shown and answered
             // The "questions" div will be hiden, and the "end-screen" div is shown.
             if (k === quizLength) {
                 endScreen.setAttribute('class','start');
                 endScreen.setAttribute('data-state','hide');
                 questions.setAttribute('class','hide');
+                clearInterval(timeInterval);
                 return;
             }
-            console.log(k);
+            
             button1.textContent = quiz[k].option1;
             button2.textContent = quiz[k].option2;
             button3.textContent = quiz[k].option3;
@@ -98,16 +115,10 @@ function answerQuestion(event){
             userQuestion.textContent = quiz[k].input;
         })
     }
-
-    // Set coundown function
-    var timeInterval = setInterval(function(){
-        timeLeft--;
-        time.innerHTML = timeLeft;
-        if (timeLeft === 0){
-            // Stop execution of time countdown
-            clearInterval(timeInterval);
-        }
-    },1000);
+    console.log("Final value of k: " + k);
+    
+    
+    
 };
 
 // Function to set time of the notice "Wrong!" or "Correct. Congratulations!" if their answer is correct or not
