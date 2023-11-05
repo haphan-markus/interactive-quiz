5345917a21e2aeb7267ee9f6ff780b0e0ac594ef
Ha Phan <ha.markus.phan@gmail.com>
2023-11-05 21:04:40 +0000

    add time countdown function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,11 +3,19 @@ var startScreen = document.getElementById("start-screen");
 var questions = document.getElementById("questions");
 var questionTitle = document.getElementById("question-title");
 var choices = document.getElementById("choices");
+var endScreen = document.getElementById("end-screen");
 
 var quizLength = quiz.length;
 console.log("Total number of questions in the quiz: " + quizLength);
 
-startButton.addEventListener('click',function(event){
+// Show time left for answering and show countdown
+var timeLeft = 120;
+var time = document.querySelector('#time');
+time.innerHTML = timeLeft;
+
+startButton.addEventListener('click',answerQuestion);
+
+function answerQuestion(event){
     // Hide start-screen div
     var startScreenState = startScreen.getAttribute('data-state');
     var startScreenClass = startScreen.getAttribute('class');
@@ -25,47 +33,63 @@ startButton.addEventListener('click',function(event){
     questions.setAttribute('class',questionState);
     
     // Show questions
-    var userQuestion = document.createElement('p');
+    var userQuestion = document.createElement('p');// Display the question from quiz
     userQuestion.innerHTML = quiz[0].input;
     questionTitle.append(userQuestion);
     userQuestion.setAttribute('class','start');
     
-    var button1 = document.createElement('button');
+    var button1 = document.createElement('button');// Create button for option 1 and insert content
     button1.innerHTML = quiz[0].option1;
     choices.append(button1);
     button1.setAttribute('data-state','bttnOption');
         
-    var button2 = document.createElement('button');
+    var button2 = document.createElement('button');// Create button for option 2 and insert content
     button2.innerHTML = quiz[0].option2;
     choices.append(button2);
     button2.setAttribute('data-state','bttnOption');
 
-    var button3 = document.createElement('button');
+    var button3 = document.createElement('button');// Create button for option 3 and insert content
     button3.innerHTML = quiz[0].option3;
     choices.append(button3);
     button3.setAttribute('data-state','bttnOption');
     
-    var button4 = document.createElement('button');
+    var button4 = document.createElement('button');// Create button for option 4 and insert content
     button4.innerHTML = quiz[0].option4;
     choices.append(button4);
     button4.setAttribute('data-state','bttnOption');
 
-    var k = 0;
-    var i = 0;
+    var k = 0; // k indicates the question number in "quiz" array
+    var correctAnswer = 0;
+    var wrongAnswer = 0;
+    var i = 0; 
     var userChoice = [button1, button2, button3, button4];
+    
     for (let j = 0; j < userChoice.length; j++){
         let paraEl = document.createElement('p');
         
         userChoice[j].addEventListener('click',function(event){
             if (userChoice[j].textContent === quiz[k].correctAnswer){
                 paraEl.innerText = "Correct. Congratulations!"
+                correctAnswer ++;
+                console.log('Total correct answers:' + correctAnswer);
                 } else {
-                paraEl.innerText = "Wrong!"
+                paraEl.innerText = "Wrong!";
+                wrongAnswer ++;
+                console.log("Total wrong answers: " + wrongAnswer);
             }
-            questions.appendChild(paraEl); // Put the p element to the end of id "questions"
+            questions.appendChild(paraEl); // Put the "p" element to the end of id "questions"
             setTime(paraEl);
-            //Upon clicking, the next question will appear
+            // Increase the value of k to switch to new Question
             k ++;
+
+            // Add if conditional statement to break this loop when all questions have been shown and answered
+            // The "questions" div will be hiden, and the "end-screen" div is shown.
+            if (k === quizLength) {
+                endScreen.setAttribute('class','start');
+                endScreen.setAttribute('data-state','hide');
+                questions.setAttribute('class','hide');
+                return;
+            }
             console.log(k);
             button1.textContent = quiz[k].option1;
             button2.textContent = quiz[k].option2;
@@ -74,12 +98,23 @@ startButton.addEventListener('click',function(event){
             userQuestion.textContent = quiz[k].input;
         })
     }
-});
 
-function setTime(i){
-    var secondLeft = 3;
-    i.setAttribute('class','');
+    // Set coundown function
     var timeInterval = setInterval(function(){
+        timeLeft--;
+        time.innerHTML = timeLeft;
+        if (timeLeft === 0){
+            // Stop execution of time countdown
+            clearInterval(timeInterval);
+        }
+    },1000);
+};
+
+// Function to set time of the notice "Wrong!" or "Correct. Congratulations!" if their answer is correct or not
+function setTime(i){
+    var secondLeft = 2;
+    i.setAttribute('class','');
+    setInterval(function(){
         secondLeft--;
         if (secondLeft === 0){
             clearInterval();
